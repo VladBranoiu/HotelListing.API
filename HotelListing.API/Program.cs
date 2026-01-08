20d9047c0d5af05fadf0248eee7995df3929ba25
Vlad Branoiu <vbranoiu111@gmail.com>
2026-01-08 15:24:39 +0200

    Addes Countries and Hotels Endpoints

--- HotelListing.API/Program.cs
+++ HotelListing.API/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddDbContext<HotelListingDbContext>(options =>
 {
     options.UseSqlServer(connectionString);
 });
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
